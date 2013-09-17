4a9bba13f99dd97be6b4b86301e27cd4c53bc84b
CDawg <clintellyson@hotmail.com>
2013-09-16 21:46:22 -0800

    Core/Vehicle: Removing vehicle debugging commands

--- src/server/scripts/Commands/cs_debug.cpp
+++ src/server/scripts/Commands/cs_debug.cpp
@@ -82,9 +82,6 @@ public:
             { "setaurastate",  RBAC_PERM_COMMAND_DEBUG_SETAURASTATE,  false, &HandleDebugSetAuraStateCommand,     "", NULL },
             { "setitemvalue",  RBAC_PERM_COMMAND_DEBUG_SETITEMVALUE,  false, &HandleDebugSetItemValueCommand,     "", NULL },
             { "setvalue",      RBAC_PERM_COMMAND_DEBUG_SETVALUE,      false, &HandleDebugSetValueCommand,         "", NULL },
-            { "spawnvehicle",  RBAC_PERM_COMMAND_DEBUG_SPAWNVEHICLE,  false, &HandleDebugSpawnVehicleCommand,     "", NULL },
-            { "setvid",        RBAC_PERM_COMMAND_DEBUG_SETVID,        false, &HandleDebugSetVehicleIdCommand,     "", NULL },
-            { "entervehicle",  RBAC_PERM_COMMAND_DEBUG_ENTERVEHICLE,  false, &HandleDebugEnterVehicleCommand,     "", NULL },
             { "uws",           RBAC_PERM_COMMAND_DEBUG_UWS,           false, &HandleDebugUpdateWorldStateCommand, "", NULL },
             { "update",        RBAC_PERM_COMMAND_DEBUG_UPDATE,        false, &HandleDebugUpdateCommand,           "", NULL },
             { "itemexpire",    RBAC_PERM_COMMAND_DEBUG_ITEMEXPIRE,    false, &HandleDebugItemExpireCommand,       "", NULL },
@@ -833,106 +830,6 @@ public:
             ref = ref->next();
         }
         handler->SendSysMessage("End of hostil reference list.");
-        return true;
-    }
-
-    static bool HandleDebugSetVehicleIdCommand(ChatHandler* handler, char const* args)
-    {
-        Unit* target = handler->getSelectedUnit();
-        if (!target || target->IsVehicle())
-            return false;
-
-        if (!args)
-            return false;
-
-        char* i = strtok((char*)args, " ");
-        if (!i)
-            return false;
-
-        uint32 id = (uint32)atoi(i);
-        //target->SetVehicleId(id);
-        handler->PSendSysMessage("Vehicle id set to %u", id);
-        return true;
-    }
-
-    static bool HandleDebugEnterVehicleCommand(ChatHandler* handler, char const* args)
-    {
-        Unit* target = handler->getSelectedUnit();
-        if (!target || !target->IsVehicle())
-            return false;
-
-        if (!args)
-            return false;
-
-        char* i = strtok((char*)args, " ");
-        if (!i)
-            return false;
-
-        char* j = strtok(NULL, " ");
-
-        uint32 entry = (uint32)atoi(i);
-        int8 seatId = j ? (int8)atoi(j) : -1;
-
-        if (!entry)
-            handler->GetSession()->GetPlayer()->EnterVehicle(target, seatId);
-        else
-        {
-            Creature* passenger = NULL;
-            Trinity::AllCreaturesOfEntryInRange check(handler->GetSession()->GetPlayer(), entry, 20.0f);
-            Trinity::CreatureSearcher<Trinity::AllCreaturesOfEntryInRange> searcher(handler->GetSession()->GetPlayer(), passenger, check);
-            handler->GetSession()->GetPlayer()->VisitNearbyObject(30.0f, searcher);
-            if (!passenger || passenger == target)
-                return false;
-            passenger->EnterVehicle(target, seatId);
-        }
-
-        handler->PSendSysMessage("Unit %u entered vehicle %d", entry, (int32)seatId);
-        return true;
-    }
-
-    static bool HandleDebugSpawnVehicleCommand(ChatHandler* handler, char const* args)
-    {
-        if (!*args)
-            return false;
-
-        char* e = strtok((char*)args, " ");
-        char* i = strtok(NULL, " ");
-
-        if (!e)
-            return false;
-
-        uint32 entry = (uint32)atoi(e);
-
-        float x, y, z, o = handler->GetSession()->GetPlayer()->GetOrientation();
-        handler->GetSession()->GetPlayer()->GetClosePoint(x, y, z, handler->GetSession()->GetPlayer()->GetObjectSize());
-
-        if (!i)
-            return handler->GetSession()->GetPlayer()->SummonCreature(entry, x, y, z, o);
-
-        uint32 id = (uint32)atoi(i);
-
-        CreatureTemplate const* ci = sObjectMgr->GetCreatureTemplate(entry);
-
-        if (!ci)
-            return false;
-
-        VehicleEntry const* ve = sVehicleStore.LookupEntry(id);
-
-        if (!ve)
-            return false;
-
-        Creature* v = new Creature;
-
-        Map* map = handler->GetSession()->GetPlayer()->GetMap();
-
-        if (!v->Create(sObjectMgr->GenerateLowGuid(HIGHGUID_VEHICLE), map, handler->GetSession()->GetPlayer()->GetPhaseMask(), entry, id, handler->GetSession()->GetPlayer()->GetTeam(), x, y, z, o))
-        {
-            delete v;
-            return false;
-        }
-
-        map->AddToMap(v->ToCreature());
-
         return true;
     }
 
